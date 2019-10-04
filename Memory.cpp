371824d1773b176e75de11c793a087f4c1fe587b
Bruno Castro <bruno.f.castro12@hotmail.com>
2019-10-04 20:34:56 -0300

    feat: load fonts into memory

--- Memory.cpp
+++ Memory.cpp
@@ -8,6 +8,10 @@
 
 namespace fs = std::filesystem;
 
+Memory::Memory() {
+	std::copy(FONTSET.begin(), FONTSET.end(), page.begin());
+}
+
 void Memory::loadRom(std::string path) {
 	std::ifstream rom(path, std::ios::in | std::ios::binary);
 	std::vector<uint8_t> data(std::istream_iterator<uint8_t>(rom), {});
