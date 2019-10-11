1be1bb8eb037523d17649ca71689d04f2bff6009
Bruno Castro <bruno.f.castro12@hotmail.com>
2019-10-11 12:24:11 -0300

    fix: properly load rom file considering white spaces

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,10 +1,7 @@
 #include "Memory.h"
 #include <fstream>
 #include <iterator>
-#include <filesystem>
 #include <iostream>
-
-namespace fs = std::filesystem;
 
 Memory::Memory() {
     std::copy(FONTSET.begin(), FONTSET.end(), page.begin());
@@ -12,19 +9,12 @@ Memory::Memory() {
 
 void Memory::loadRom(std::string path) {
     std::ifstream rom(path, std::ios::in | std::ios::binary);
-    std::vector<uint8_t> data(std::istream_iterator<uint8_t>(rom), {});
+    std::vector<uint8_t> data((std::istreambuf_iterator<char>(rom)), std::istreambuf_iterator<char>());
 
-    try {
-        auto size = fs::file_size(path);
-
-        if (size > MAX_PROGRAM_SIZE) {
-            std::cout << "Program exceeds maximum size of " << MAX_PROGRAM_SIZE << std::endl;
-        }
-        else {
-            std::copy(data.begin(), data.end(), page.begin() + PROGRAM_SPACE);
-        }
+    if (data.size() > MAX_PROGRAM_SIZE) {
+        std::cout << "Program exceeds maximum size of " << MAX_PROGRAM_SIZE << std::endl;
     }
-    catch (fs::filesystem_error & e) {
-        std::cout << e.what() << '\n';
+    else {
+        std::copy(data.begin(), data.end(), page.begin() + PROGRAM_SPACE);
     }
 }
